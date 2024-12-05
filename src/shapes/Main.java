e7c0596c043f5dab848c5878ef933af09774e061
Anthony Gough <a1.gough@qut.edu.au>
2024-12-06 00:12:08 +1000

    Point class creation - start of Shape Exercise

---
+++ src/shapes/Main.java
@@ -0,0 +1,14 @@
+package shapes;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Point point = new Point(3,3);
+        Point point2 = new Point(4,4);
+        double dis = point.distanceBetween(point2);
+        System.out.println(point.toString());
+        System.out.println(point2.toString());
+        System.out.println("Double: " + String.format("%.2f", dis));
+        }
+    }
